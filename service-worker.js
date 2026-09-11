fd27e9dc45e2f63d14e26e5d5d82258296f105ce
Jackiexu0619 <356178672@qq.com>
2026-09-11 22:00:01 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,26 @@
-const CACHE='pet-checkin-v0.1.3';
+const CACHE='pet-checkin-v0.2.0';
 const ASSETS=['./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){
-  e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));
- }else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  if(e.request.mode==='navigate'){
+    e.respondWith(
+      fetch(e.request)
+        .then(r=>{
+          const copy=r.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return r;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
 });
