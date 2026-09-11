14b1be9f5f6455c5129db2ecf26f3b2b3313072d
Jackiexu0619 <356178672@qq.com>
2026-09-11 22:29:27 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pet-checkin-v0.2.0';
+const CACHE='pet-checkin-v0.2.1';
 const ASSETS=['./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
